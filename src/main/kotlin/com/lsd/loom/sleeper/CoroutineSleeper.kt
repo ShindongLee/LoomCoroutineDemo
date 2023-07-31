dfd747b9b93944da2ec259568abd2719abb8d46b
shindong.lee <shindong.lee@toss.im>
2023-07-31 11:15:53 +0900

    CoroutineSleeper 추가

---
+++ src/main/kotlin/com/lsd/loom/sleeper/CoroutineSleeper.kt
@@ -0,0 +1,48 @@
+package com.lsd.loom.sleeper
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.springframework.stereotype.Service
+import java.util.concurrent.Executors
+import kotlin.time.Duration
+import kotlin.time.TimeSource
+
+@Service
+class CoroutineSleeper {
+    @OptIn(ExperimentalCoroutinesApi::class)
+    private val ioDispatcher = Dispatchers.IO.limitedParallelism(10_000_000)
+    private val loomDispatcher = Executors.newVirtualThreadPerTaskExecutor().asCoroutineDispatcher()
+
+    fun sleepByIoDispatcher(sleepCount: Int, isBlocking: Boolean): Duration {
+        val mark = TimeSource.Monotonic.markNow()
+        runBlocking {
+            repeat(sleepCount) {
+                launch(ioDispatcher) { sleep(isBlocking) }
+            }
+        }
+        return mark.elapsedNow()
+    }
+
+    fun sleepByLoomDispatcher(sleepCount: Int, isBlocking: Boolean): Duration {
+        val mark = TimeSource.Monotonic.markNow()
+        runBlocking {
+            repeat(sleepCount) {
+                launch(loomDispatcher) { sleep(isBlocking) }
+            }
+        }
+        return mark.elapsedNow()
+    }
+
+    private suspend fun sleep(isBlocking: Boolean) {
+        if (isBlocking)
+            Thread.sleep(1000L)
+        else
+            delay(1000L)
+    }
+}
